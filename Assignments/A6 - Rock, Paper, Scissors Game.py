db95f960c1c203f639d0a43b02e7a5893cba0c5a
Jeremy Stretter <43216926+Afrodisiac@users.noreply.github.com>
2023-02-27 21:01:07 -0800

    Initial commit of A6

---
+++ Assignments/A6 - Rock, Paper, Scissors Game.py
@@ -0,0 +1,34 @@
+# A5 - ATM Program by Jeremy Stretter
+# COMP B10
+# Professor Moseley
+# Spring 2023
+# 2/22/2023
+
+# Constraints:
+    # The game should be player vs. computer
+    # There should be at least one function used in your code.
+    # The game should go through up to three rounds,
+    # and declare the winner after either the computer or the player wins 2 rounds.
+    # After a winner is declared, the program should ask if the user wants to play another game.
+
+
+# Import random module
+import random
+
+
+# Program
+def main():
+    # Welcome message/UI
+    # To-do:
+    #   - UI
+    
+    # Initializes scores
+    intPlayer_Score = 0
+    intComputer_Score = 0
+
+    # Logic
+    
+
+    # Rounds
+
+
